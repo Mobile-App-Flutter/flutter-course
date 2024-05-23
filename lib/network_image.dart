c0be1944b48921c1ec8b8533da49cb8e4ee99b90
sachindu <sachindu@thesanmark.com>
2024-05-23 21:46:12 +0530

    #2 Scroll

--- lib/network_image.dart
+++ lib/network_image.dart
@@ -8,23 +8,43 @@ class NetworkImagePage extends StatelessWidget {
     var displaySize = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
-        child: Column(
-          children: [
-            Image.network(
-              'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',
-              height: displaySize.height / 3,
-              loadingBuilder: (context, child, loadingProgress) {
-                if (loadingProgress == null) return child;
-                return Container(
-                    height: displaySize.height / 3,
-                    child: Center(child: LinearProgressIndicator()));
-              },
-            ),
-            // CircularProgressIndicator(),
-            // LinearProgressIndicator()
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              CustomNetworkImage(displaySize: displaySize),
+              CustomNetworkImage(displaySize: displaySize),
+              CustomNetworkImage(displaySize: displaySize),
+              CustomNetworkImage(displaySize: displaySize),
+              CustomNetworkImage(displaySize: displaySize),
+              // CircularProgressIndicator(),
+              // LinearProgressIndicator()
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class CustomNetworkImage extends StatelessWidget {
+  const CustomNetworkImage({
+    super.key,
+    required this.displaySize,
+  });
+
+  final Size displaySize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',
+      height: displaySize.height / 3,
+      loadingBuilder: (context, child, loadingProgress) {
+        if (loadingProgress == null) return child;
+        return Container(
+            height: displaySize.height / 3,
+            child: Center(child: LinearProgressIndicator()));
+      },
     );
   }
 }
